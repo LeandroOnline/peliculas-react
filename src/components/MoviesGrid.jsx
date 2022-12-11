3d3fdde8b042a9d812c02b00bd36bb9e85cd79d0
LeandroOnline <105742869+LeandroOnline@users.noreply.github.com>
2022-12-11 20:36:53 -0300

    m

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import MovieCard from './MovieCard';
-import movies from "../movies.json";
-import styles from "./MovieGrid.module.css"
+import React, { useEffect, useState } from "react";
+import Get from "../utilities/get";
+import MovieCard from "./MovieCard";
+// import movies from "../movies.json";
+import styles from "./MovieGrid.module.css";
 
 const MoviesGrid = () => {
-    return (
-        <ul className={styles.movieGrid}>
-            {movies.map((movie) => (<MovieCard key={movie.id} movie={movie} />))}
-        </ul>
-    );
-}
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    Get("/discover/movie").then((data) => setMovies(data.results));
+  }, []);
+
+  return (
+    <ul className={styles.movieGrid}>
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
+    </ul>
+  );
+};
 
 export default MoviesGrid;
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
-import movie from "../movies copy.json";
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Get from '../utilities/get';
+// import movie from "../movies copy.json";
 import styles from "./MovieDetails.module.css";
 
 const MovieDetails = () => {
+    
+    const {movieId} = useParams(); //es el parametro que esta en la ruta especificada en App
+    const [movie, setMovie] = useState(null);
+    
+    useEffect(() => {
+       Get("/movie/" + movieId).then(data=>setMovie(data));
+    }, [movieId]);
+
+    if(!movie) return null;
+
     const imageUrl="https://image.tmdb.org/t/p/w300" + movie.poster_path;
+
     return (
         <div className={styles.detail}>
             <img  className={styles.col +" "+styles.image} src={imageUrl} alt="" />
