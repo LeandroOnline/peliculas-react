6f6f87935ca417dcf6e7c79aae554d49611e75f9
LeandroOnline <105742869+LeandroOnline@users.noreply.github.com>
2022-12-12 19:15:42 -0300

    m

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -17,13 +17,6 @@ const Search = () => {
         <form className={styles.container} onSubmit={handlerSubmit}>
             <div className={styles.box}>
                 <input placeholder="Movie to search" type="text" className={styles.input} value={input} onChange={(e)=>setInput(e.target.value)}/>
-                <div className={styles.icon}>
-                    <svg viewBox="0 0 512 512" className={styles.icon} xmlns="http://www.w3.org/2000/svg">
-                        <title>Search</title>
-                        <path stroke-width="32" stroke-miterlimit="10" stroke="currentColor" fill="none" d="M221.09 64a157.09 157.09 0 10157.09 157.09A157.1 157.1 0 00221.09 64z"></path>
-                        <path d="M338.29 338.29L448 448" stroke-width="32" stroke-miterlimit="10" stroke-linecap="round" stroke="currentColor" fill="none"></path>
-                    </svg>
-                </div>
             </div>
         </form> 
     );
